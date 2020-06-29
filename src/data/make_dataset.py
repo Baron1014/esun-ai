a3dc2c17b7310a729b8ac25c5bf07a9aea4a041e
Baron <04170133@gm.scu.edu.tw>
2020-06-30 00:22:30 +0800

    merge training data & crawler data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,7 +7,7 @@ def main():
 
 
 def _pdjson():
-    json_path = "crawler/test.json"
+    json_path = "crawler/output.json"
     csv_save_path = "../data/processed"
     with open(json_path, encoding="utf-8") as f_input:
         dfjson = pd.read_json(f_input)
